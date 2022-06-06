2eedb696958a5a8f7930c8b359aaa63271d9425d
Hugo <alheiodef@live.com>
2022-06-06 19:46:01 -0300

    primeiraversao

---
+++ jokenpo.js
@@ -0,0 +1,13 @@
+let userInput = prompt("escolha : pedra, papel ou tesoura.")
+
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'pedra'   userInput === 'papel'   userInput === 'tesoura') {
+    } 
+    else {
+        console.log('Escolha inválida');
+    }
+
+}
+ getUserChoice(userInput)
+
