ec52d8783b44afe38b9e82c5b1f28dd6b43ef335
Hugo <alheiodef@live.com>
2022-06-07 21:51:53 -0300

    ultimaversao

--- jokenpo.js
+++ jokenpo.js
@@ -1,15 +1,16 @@
-let userInput = prompt("escolha : pedra, papel ou tesoura.")
-
-const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'pedra'   userInput === 'papel'   userInput === 'tesoura') {
-    } 
-    else {
-        console.log('Escolha inválida');
+const getUserChoice = (userInput) => { 
+    if(userInput.toLocaleLowerCase() === 'pedra'){
+        userInput = 'pedra'
+    }else if(userInput.toLocaleLowerCase() === 'papel'){
+        userInput = 'papel'
+    }else if(userInput.toLocaleLowerCase() === 'tesoura'){
+        userInput = 'tesoura'
+    }else{
+        console.log("A opção está inválida!")
     }
-
+    return userInput
 }
- getUserChoice(userInput)
+
 
 function  getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -22,3 +23,26 @@ function  getComputerChoice() {
     }
 }
 
+function determineWinner (userChoice, computerChoice) {
+    if (userChoice == computerChoice) {
+        return 'Empate';
+    } else if (userChoice == 'pedra' && computerChoice == 'tesoura') {
+        return 'Você ganhou';
+    }else if (userChoice == 'papel' && computerChoice == 'pedra') {
+        return 'Você ganhou';
+    } else if (userChoice == 'tesoura' && computerChoice == 'papel') {
+        return 'Você ganhou';
+    }else {
+        return 'Você perdeu';
+    }
+    }
+
+    function playGame() {
+        let userChoice = getUserChoice(prompt("escolha : pedra, papel ou tesoura."));
+        let computerChoice = getComputerChoice();
+        console.log('Usuário escolheu: ' + userChoice);
+        console.log('Computador escolheu: ' + computerChoice);
+        console.log(determineWinner (userChoice, computerChoice))
+    }
+    
+playGame()
